e969959a77c4083dcbe52aabae936b29dc801341
John Seagull <149725024+thecreeper3326@users.noreply.github.com>
2026-09-10 20:58:56 -0400

    added an escape for hidden configs

--- src/client/java/johnseagull/figManagerClient/FigScreen.java
+++ src/client/java/johnseagull/figManagerClient/FigScreen.java
@@ -104,7 +104,7 @@ public class FigScreen<T extends AbstractWidget & Renderable> extends Screen {
         for(Field field : this.fieldsInFigs) {
             field.setAccessible(true);
             Object value = field.get(FigManager.FIGS);
-            if (!field.getName().equals("instance")) {
+            if (!field.getName().equals("instance") && !field.getName().startsWith("fmc_H_")) {
                 if (value instanceof FigGroup) {
                     FigGroup t = (FigGroup)value;
                     int c = t.columns;
